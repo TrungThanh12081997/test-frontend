90b67ebe52e215090176420c79795a91aeaa43fb
quanthuyduyen <quanthuyduyen@gmail.com>
2023-06-29 12:47:25 +0700

    config

--- src/store/provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Provider } from "react-redux";
-import { store } from "./index";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-    console.log("test store");
-    return <Provider store={store}>{children}</Provider>;
-}
